3156ee990fd94e2a7e014980ac4f832b3c80fa64
mushfiqbh <mushfiqbh@gmail.com>
2025-09-24 20:11:25 +0600

    chore: Laravel + Vite Project Setup & Database Migration Completed

---
+++ database/migrations/2025_09_24_133846_create_careers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->text('description')->nullable();
+            $table->text('requirements')->nullable();
+            $table->string('location', 100)->nullable();
+            $table->enum('employment_type', ['Full-time','Part-time','Internship','Contract'])->nullable();
+            $table->enum('status', ['Open','Closed'])->default('Open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('careers');
+    }
+};
---
+++ database/migrations/2025_09_24_133914_create_blog_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 200);
+            $table->string('author', 100)->nullable();
+            $table->date('published_date')->nullable();
+            $table->string('image')->nullable();
+            $table->longText('content')->nullable();
+            $table->integer('likes')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog');
+    }
+};
